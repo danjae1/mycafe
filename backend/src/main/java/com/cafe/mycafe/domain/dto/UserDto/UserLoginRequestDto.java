55637222b794620a81ed4fff7dbe0bcaa2d019ce
danjae1 <numbness98@naver.com>
2025-09-23 23:59:26 +0900

    added modif entity&repository 3

--- backend/src/main/java/com/cafe/mycafe/domain/dto/UserDto/UserLoginRequestDto.java
+++ backend/src/main/java/com/cafe/mycafe/domain/dto/UserDto/UserLoginRequestDto.java
@@ -1,4 +1,13 @@
 package com.cafe.mycafe.domain.dto.UserDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserLoginRequestDto {
+    private String username;
+    private String password;
 }
